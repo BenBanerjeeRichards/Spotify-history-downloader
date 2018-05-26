238ab9cfd2d9a6b882d71c4f2268e78594eafe78
Ben Banerjee-Richards <dev@benbr.com>
2018-05-26 20:06:17 +0100

    info to events

--- analysis/gen_events.py
+++ analysis/gen_events.py
@@ -37,7 +37,7 @@ def gen_events():
         if "repeat_state" in state:
             if is_repeat != state["repeat_state"]:
                 new_event = True
-                event["is_repeat"] = state["repeat_state"]
+                event["repeat"] = state["repeat_state"]
                 is_repeat = state["repeat_state"]
 
         if "device" in state:
@@ -46,17 +46,47 @@ def gen_events():
                 event["device"] = state["device"]["name"]
                 device = state["device"]["name"]
 
-
-
-
         if new_event:
             event["timestamp"] = state["timestamp"]
             events.append(event)
     return events
 
 
+def add_info_to_events(events):
+    client = pymongo.MongoClient("localhost", 27017)
+    spotify = client.spotify
+    track_cache = {}
+
+    for event in events:
+        if "track_id" in event:
+            t_id = event["track_id"]
+
+            if t_id in track_cache:
+                event["track"] = track_cache[t_id]["track"]
+                event["artist"] = track_cache[t_id]["artist"]
+            else:
+                track_info = spotify.tracks.find_one({"id": t_id})
+                if track_info is None:
+                    # If new track not in db (definitly possible, recently played requires > 30 secs)
+                    print("Failed to get track info for track {}".format(t_id))
+                else:
+                    info = {
+                        "track": track_info["name"],
+                        "artist": track_info["artists"][0]["name"]
+                    }
+                    event["track"] = info["track"]
+                    event["artist"] = info["artist"]
+                    track_cache[t_id] = info
+
+
+
+
+
+
 def main():
-    for e in gen_events():
+    events = gen_events()
+    add_info_to_events(events)
+    for e in events:
         print(e)
 
 if __name__=="__main__":main()
