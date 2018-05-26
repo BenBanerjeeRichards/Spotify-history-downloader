7625edd769ed356690f2999a74c2d1434de354e7
Ben Banerjee-Richards <dev@benbr.com>
2018-05-26 19:33:26 +0100

    Gen events from player states

---
+++ analysis/gen_events.py
@@ -0,0 +1,37 @@
+import pymongo
+
+def gen_events():
+    client = pymongo.MongoClient("localhost", 27017)
+    spotify = client.spotify
+
+    is_playing = None
+    track_id = None
+
+    states = spotify.player.find({}, sort=[("timestamp", pymongo.ASCENDING)])
+    events = []
+
+    for state in states:
+        event = {}
+        new_event = False
+
+        if is_playing != state["is_playing"]:
+            new_event = True
+            event["is_playing"] =  state["is_playing"]
+            is_playing = state["is_playing"]
+
+        if "track_id" in state:
+            if track_id != state["track_id"]:
+                new_event = True
+                event["track_id"] = state["track_id"]
+                track_id = state["track_id"]
+
+        if new_event:
+            events.append(event)
+    return events
+
+
+def main():
+    for e in gen_events():
+        print(e)
+
+if __name__=="__main__":main()
