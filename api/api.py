7026a50d60fff431b63a0a8ccb60b51a2a22ff3a
Ben Banerjee-Richards <contact@benbr.com>
2018-11-22 22:12:03 +0000

    Now playing API

--- api/api.py
+++ api/api.py
@@ -1,15 +1,16 @@
 from bottle import response, run, hook, route
 import sys
 from analysis.day import *
+import spotify
 
 
 @route("/spotify/day")
-def spotify():
+def get_stats():
     return get_stats(datetime.datetime.now())
 
 
 @route("/spotify/day/<year>/<month>/<day>")
-def spotify(year, month, day):
+def get_stats_specific(year, month, day):
     return get_stats(datetime.datetime(int(year), int(month), int(day)))
 
 
@@ -22,6 +23,43 @@ def enable_cors():
     response.headers['Access-Control-Allow-Origin'] = '*'
     response.headers['Access-Control-Allow-Methods'] = 'PUT, GET, POST, DELETE, OPTIONS'
     response.headers['Access-Control-Allow-Headers'] = 'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token'
+
+
+@route("/spotify/playing")
+def playing():
+    creds = spotify.get_credentials()
+    playing = spotify.get_current_playback(creds)
+    if playing is None or playing == {}:
+        return {}
+
+    if playing["device"]["is_private_session"]:
+        return {}
+
+    track = playing["item"]
+
+    return {
+        "song": track["name"],
+        "song_link": track["href"],
+        "artist": track["artists"][0]["name"],
+        "artist_link": track["artists"][0]["href"],
+        "progress_ms": playing["progress_ms"],
+        "duration_ms": track["duration_ms"],
+        "progress": format_ms_as_str(playing["progress_ms"]),
+        "duration": format_ms_as_str(track["duration_ms"])
+
+    }
+
+
+def format_ms_as_str(ms):
+    minutes, seconds = format_ms(ms)
+    return "{}:{}".format(minutes, seconds)
+
+
+def format_ms(ms):
+    as_seconds = ms / 1000
+    minutes = int(as_seconds / 60)
+    seconds = as_seconds - (60 * minutes)
+    return minutes, int(seconds)
 
 
 def main():
