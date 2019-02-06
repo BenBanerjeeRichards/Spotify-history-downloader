f223a40ef7cbbc7da29480381dd1e21781468311
Ben Banerjee-Richards <contact@benbr.com>
2019-02-06 20:48:31 +0000

    upload

--- upload/upload.py
+++ upload/upload.py
@@ -1,12 +1,16 @@
 import os
 import datetime
+from util import config
 
 
 def run_export():
+
+    cfg = config()
+    os.chdir(cfg["upload"]["cwd"])
     # I know should use subprocess
     os.system("mongoexport --db spotify --collection tracks --out tracks.json")
     os.system("mongoexport --db spotify --collection events --out events.json")
-    os.system("git add tracks.json events.json")
+    os.system("git add tracks.json events.json art.csv art-small.csv art-medium.csv")
     os.system("git commit -m \"Data upload at {}\"".format(datetime.datetime.now()))
     os.system("git push -u origin master")
 
